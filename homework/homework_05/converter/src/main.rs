f74c27a6f16a70ac2bc95a198cb6ead3558071b2
Derek Sifford <dereksifford@gmail.com>
2019-04-07 17:36:33 -0400

    adjust homework_05 converter app

--- homework/homework_05/converter/src/main.rs
+++ homework/homework_05/converter/src/main.rs
@@ -1,35 +1,19 @@
-extern crate clap;
+use clap::clap_app;
+
+use machines::{b2u, u2b};
 
 mod machines;
 mod tm;
 
-use clap::{App, Arg};
-use machines::{b2u, u2b};
-
 fn main() {
-    let args = App::new("Converter")
-        .version("0.1.0")
-        .author("Derek Sifford <dereksifford@gmail.com>")
-        .about("Turing machines to convert binary to unary and vice-versa")
-        .arg(
-            Arg::with_name("b2u")
-                .required(true)
-                .long("b2u")
-                .short("b")
-                .takes_value(true)
-                .help("Convert from binary input")
-                .conflicts_with("u2b"),
-        )
-        .arg(
-            Arg::with_name("u2b")
-                .required(true)
-                .long("u2b")
-                .short("u")
-                .takes_value(true)
-                .help("Convert from unary input")
-                .conflicts_with("b2u"),
-        )
-        .get_matches();
+    let args = clap_app!(Converter =>
+        (version: "0.1.0")
+        (author: "Derek Sifford <dereksifford@gmail.com>")
+        (about: "Turing machines to convert binary to unary and vice-versa")
+        (@arg b2u: -b --b2u +required +takes_value conflicts_with[u2b] "Convert from binary input")
+        (@arg u2b: -u --u2b +required +takes_value conflicts_with[b2u] "Convert from unary input")
+    )
+    .get_matches();
 
     if args.is_present("b2u") {
         let value: i32 = args.value_of("b2u").unwrap().parse().unwrap();
